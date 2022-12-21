ff712bdf6276e1cbc0d015231f5444dc07c55bd3
Gibsol <gibsol75@gmail.com>
2022-12-21 19:57:14 +0200

    Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
-import '../styles/globals.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
