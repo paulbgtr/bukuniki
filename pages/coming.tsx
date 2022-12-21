f752a48e9bf37f620a75354dca7074cdee56d7c2
Gibsol <gibsol75@gmail.com>
2022-12-21 19:58:32 +0200

    Coming soon page Added the Head component and styled the page the tailwind way

--- pages/coming.tsx
+++ pages/coming.tsx
@@ -1,25 +1,33 @@
-import Container from "react-bootstrap/Container";
+import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Coming.module.scss";
 
 const ComingSoon = () => {
   return (
-    <main className="main">
-      <Container className="text-center">
-        <h1 className={styles.title}>Coming Soon...</h1>
-        <p>
-          Bukuniki is currently under active development. <br />
-          You can follow the development process on our{" "}
-          <Link
-            className="main-link"
-            href={"https://github.com/gibsol/bukuniki"}
-          >
-            Github
-          </Link>{" "}
-          page.
-        </p>
-      </Container>
-    </main>
+    <>
+      <Head>
+        <title>bukuniki / coming soon</title>
+        <meta name="description" content="Online readin journal." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <section className="text-center">
+          <h1 className="text-7xl font-bold">Coming Soon...</h1>
+          <p className="mt-4">
+            Bukuniki is currently under active development. <br />
+            You can follow the development process on our{" "}
+            <Link
+              className="main-link"
+              href={"https://github.com/gibsol/bukuniki"}
+            >
+              Github
+            </Link>{" "}
+            page.
+          </p>
+        </section>
+      </main>
+    </>
   );
 };
 
