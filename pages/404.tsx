c18b60c39311d79805b735ebdc357826e51c332a
Gibsol <gibsol75@gmail.com>
2022-12-21 19:57:30 +0200

    Removed bootstrap container

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,15 @@
-import Container from "react-bootstrap/Container";
 import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
     <main className="main">
-      <Container className="text-center">
-        <h1 className="title">404</h1>
-        <p>
-          The page you were looking for could not be found. <br />
-          <Link className="main-link" href={"/"}>
-            Go back home
-          </Link>
-        </p>
-      </Container>
+      <h1 className="title">404</h1>
+      <p>
+        The page you were looking for could not be found. <br />
+        <Link className="main-link" href={"/"}>
+          Go back home
+        </Link>
+      </p>
     </main>
   );
 };
