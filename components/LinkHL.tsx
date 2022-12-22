45e15b721fc9b492ca4932b031b8f1c1ed6c9ef0
Gibsol <gibsol75@gmail.com>
2022-12-22 11:37:51 +0200

    Link Highlighter
    This is a simple component that highlights the link when the user hovers over it.

---
+++ components/LinkHL.tsx
@@ -0,0 +1,22 @@
+// Highlighted link component
+import { FC } from "react";
+import Link from "next/link";
+
+interface Props {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+const LinkHL: FC<Props> = (props) => {
+  return (
+    <Link
+      className={`${props.className} text-lg text-pink-300 no-underline font-bold duration-300 hover:text-white`}
+      href={props.href}
+    >
+      {props.children}
+    </Link>
+  );
+};
+
+export default LinkHL;
