c4bce74e5e6bc30f669d258e0c21ac8b5c3feb9d
Gibsol <gibsol75@gmail.com>
2022-12-22 11:17:57 +0200

    404 page update Added head and my to the container

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,28 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <main className="main">
-      <h1 className="title">404</h1>
-      <p>
-        The page you were looking for could not be found. <br />
-        <Link className="main-link" href={"/"}>
-          Go back home
-        </Link>
-      </p>
-    </main>
+    <>
+      <Head>
+        <title>bukuniki / 404</title>
+        <meta name="description" content="Online readin journal." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="text-center my-20">
+          <h1 className="text-7xl font-bold">404</h1>
+          <p>
+            The page you were looking for could not be found. <br />
+            <Link className="main-link" href={"/"}>
+              Go back home
+            </Link>
+          </p>
+        </div>
+      </main>
+    </>
   );
 };
 
