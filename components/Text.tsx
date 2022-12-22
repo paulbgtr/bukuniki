073e1be7739a34cca3c862db404ad0cda446170a
Gibsol <gibsol75@gmail.com>
2022-12-22 11:38:15 +0200

    Text component
    This component is used to display text on the screen. It is a wrapper around the HTML < p > tag.

---
+++ components/Text.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Text: FC<Props> = (props) => {
+  return <p className={`${props.className} text-lg`}>{props.children}</p>;
+};
+
+export default Text;
