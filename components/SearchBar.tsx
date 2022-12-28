d20a9dce8ea5f5d7fcc51bd618a46daf69b8fd12
Gibsol <gibsol75@gmail.com>
2022-12-28 11:33:31 +0200

    SearchBar component boilerplate

---
+++ components/SearchBar.tsx
@@ -0,0 +1,11 @@
+const SearchBar = () => {
+  return (
+    <input
+      placeholder="Search for journals..."
+      className="w-96 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+      type="text"
+    />
+  );
+};
+
+export default SearchBar;
