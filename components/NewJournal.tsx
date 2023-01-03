ecd5904cec8082d67b76a3415734d6554f8e8bdd
Gibsol <gibsol75@gmail.com>
2023-01-03 14:22:51 +0200

    Added required fields to the inputs

--- components/NewJournal.tsx
+++ components/NewJournal.tsx
@@ -5,19 +5,23 @@ const NewJournal = () => {
   return (
     <div>
       <Card title="New Journal">
-        <input
-          placeholder="Book's Author"
-          className="w-96 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-          type="text"
-        />
-        <input
-          placeholder="Book's Title"
-          className="w-96 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-          type="text"
-        />
-        <div className="mt-5">
-          <AddJournal />
-        </div>
+        <form action="">
+          <input
+            placeholder="Book's Author"
+            className="w-96 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+            type="text"
+            required
+          />
+          <input
+            placeholder="Book's Title"
+            className="w-96 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+            type="text"
+            required
+          />
+          <div className="mt-5">
+            <AddJournal />
+          </div>
+        </form>
       </Card>
     </div>
   );
