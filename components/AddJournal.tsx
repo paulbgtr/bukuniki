58d31fee9b33c233056b4ef434d48757af10d4f7
Gibsol <gibsol75@gmail.com>
2023-01-03 14:05:01 +0200

    Toastify library and AddJournal component
    Added toastify library;
    Added notification when clicked on the AddJournal button component;
    Styled the toastify notification's dark theme;
    
    Updated some packages;

--- components/AddJournal.tsx
+++ components/AddJournal.tsx
@@ -1,10 +1,42 @@
 // Description: The Add Journal "+" button for the New Journal component.
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const AddJournal = () => {
+  const notify = () =>
+    toast("Journal Added!", {
+      position: "bottom-center",
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
   return (
-    <button className="rounded-full bg-pink-300 px-4 py-2 text-3xl text-white">
-      +
-    </button>
+    <>
+      <button
+        onClick={notify}
+        className="rounded-full bg-pink-300 px-4 py-2 text-3xl text-white hover:opacity-70 duration-300"
+      >
+        +
+      </button>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 };
 
