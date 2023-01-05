9efe4cc12f52b46c4e12c55048b8e4c1ca52d370
Gibsol <gibsol75@gmail.com>
2023-01-05 13:07:07 +0200

    Added new page for my journals

--- pages/my-journals.tsx
+++ pages/my-journals.tsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 const MyJournals = () => {
   return (
     <>
+      <Head>
+        <title>bukuniki / my journals </title>
+        <meta name="description" content="your journals of bukuniki" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Navbar />
+
       <main>
         <h1>My Journals</h1>
       </main>
