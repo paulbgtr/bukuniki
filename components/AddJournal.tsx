bb9dae6c3b13b3bce14f80c9fa2feeedbf22e631
Gibsol <gibsol75@gmail.com>
2023-01-05 12:27:28 +0200

    Removed notification from the component

--- components/AddJournal.tsx
+++ components/AddJournal.tsx
@@ -1,21 +1,8 @@
 // Description: The Add Journal "+" button for the New Journal component.
 
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { notify } from "./NewJournalNotification";
 
 const AddJournal = () => {
-  const notify = () =>
-    toast("Journal Added!", {
-      position: "bottom-center",
-      autoClose: 1000,
-      hideProgressBar: true,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "dark",
-    });
-
   return (
     <>
       <button
@@ -24,18 +11,6 @@ const AddJournal = () => {
       >
         +
       </button>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={1000}
-        hideProgressBar={true}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
     </>
   );
 };
