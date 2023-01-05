a701c5ebe6f03e855b8ad3095d94b09abaea685a
Gibsol <gibsol75@gmail.com>
2023-01-05 12:28:16 +0200

    NewJournal component conditional notification rendering
    This commit adds a conditional rendering of the NewJournalNotification component if the form is submitted.

--- components/NewJournal.tsx
+++ components/NewJournal.tsx
@@ -1,7 +1,22 @@
 import Card from "./Card";
 import AddJournal from "./AddJournal";
+import { useState } from "react";
+import NewJournalNotification from "./NewJournalNotification";
 
 const NewJournal = () => {
+  const [state, setState] = useState({
+    author: "",
+    title: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <Card title="New Journal">
@@ -10,16 +25,23 @@ const NewJournal = () => {
             placeholder="Book's Author"
             className="w-96 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
             type="text"
+            name="author"
+            value={state.author}
+            onChange={handleChange}
             required
           />
           <input
             placeholder="Book's Title"
             className="w-96 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
             type="text"
+            name="title"
+            value={state.title}
+            onChange={handleChange}
             required
           />
           <div className="mt-5">
             <AddJournal />
+            {state.author && state.title ? <NewJournalNotification /> : null}
           </div>
         </form>
       </Card>
