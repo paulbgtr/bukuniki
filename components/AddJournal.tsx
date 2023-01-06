6f34a868f0fda5bf8e48b0922bb01065c47e5ba6
Gibsol <gibsol75@gmail.com>
2023-01-06 11:11:42 +0200

    AddJournal component click handler
    Added a function that calls the notify function only if the "AddJournal" button isClicked

--- components/AddJournal.tsx
+++ components/AddJournal.tsx
@@ -1,13 +1,24 @@
 // Description: The Add Journal "+" button for the New Journal component.
 
 import React from "react";
+import { useState } from "react";
 import { notify } from "./NewJournalNotification";
 
 const AddJournal = () => {
+  const [isClicked, setIsClicked] = useState(true);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+
+    if (isClicked) {
+      notify();
+    }
+  };
+
   return (
     <>
       <button
-        onClick={notify}
+        onClick={handleClick}
         className="rounded-full bg-pink-300 px-4 pt-1 pb-2 text-3xl text-white hover:opacity-70 duration-300"
       >
         +
