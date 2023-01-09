d4b2d376e67476e24f397edd6f996a3375ce8c15
Gibsol <gibsol75@gmail.com>
2023-01-09 16:50:03 +0200

    Added a new Container component to wrap the content of the pages.
    This component will allow us to have a consistent layout across all pages.

---
+++ components/Container.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Container: FC<Props> = (props) => {
+  return (
+    <div className={`${props.className} container md:my-16 mx-auto`}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Container;
