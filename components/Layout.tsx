9f68a8cdf0d4bcfd3cf3acc2f0abc31ed9d816ac
Gibsol <gibsol75@gmail.com>
2023-01-10 18:47:27 +0200

    Layout path check
    If path is "/" then don't show navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from "react";
+import Navbar from "./Navbar";
+import { useRouter } from "next/router";
 
 type Props = {
   children: React.ReactNode;
@@ -6,8 +8,11 @@ type Props = {
 };
 
 const Layout: FC<Props> = (props) => {
+  const { asPath } = useRouter();
+
   return (
     <>
+      {asPath !== "/" && <Navbar />}
       <div className={`${props.className} container md:my-16 mx-auto`}>
         {props.children}
       </div>
