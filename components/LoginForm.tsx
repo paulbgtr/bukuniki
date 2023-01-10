9bb6d9e921edfecaef22257579ee8dcbaa4c0370
Gibsol <gibsol75@gmail.com>
2023-01-10 18:33:07 +0200

    LoginForm: states and header
    Added header for the component as well as states for the form.

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,33 +1,55 @@
-import React, { FC } from "react";
+// todo implement email validation
+
+import React, { useState, FC } from "react";
 import Button from "./Button";
 import ShortLinkText from "../components/ShortLinkText";
 
 const LoginForm: FC = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
-    <>
-      <div className="shadow-xl flex justify-center w-2/5">
-        <form className="flex justify-center flex-col mb-5 mt-5" action="">
-          <input
-            placeholder="Email"
-            className="w-80 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-            type="email"
-            name="email"
-            id="email"
-          />
-          <input
-            placeholder="Password"
-            className="w-80 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-            type="password"
-            name="password"
-            id="password"
-          />
-          <Button className="mt-3 py-2">Login</Button>
-          <ShortLinkText className="self-center mt-3" href="/signup">
-            Sign Up
-          </ShortLinkText>
-        </form>
-      </div>
-    </>
+    <div className="shadow-2xl rounded-xl flex justify-center w-2/5">
+      <form className="flex justify-center flex-col mb-5 mt-5" action="">
+        <h1 className="text-lg font-bold text-center mb-3">
+          Login to bukuniki
+        </h1>
+        <input
+          placeholder="Email"
+          className="w-80 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+          type="email"
+          value={state.email}
+          onChange={handleChange}
+          name="email"
+          id="email"
+          required
+        />
+        <input
+          placeholder="Password"
+          className="w-80 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+          type="password"
+          value={state.password}
+          onChange={handleChange}
+          name="password"
+          id="password"
+          required
+        />
+        <Button className="mt-3 py-2">Login</Button>
+        <ShortLinkText className="self-center mt-3" href="/signup">
+          Sign Up
+        </ShortLinkText>
+      </form>
+    </div>
   );
 };
 
