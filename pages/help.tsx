448096a0fa9d646f6c09c4ea9a1f1450ca558c0e
Gibsol <gibsol75@gmail.com>
2023-01-11 16:05:07 +0200

    :construction: Added a boilerplate for the future help page
    This page will be separated in several components as well as contain a contact form (and some other ways to contact us) in the future.

---
+++ pages/help.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from "react";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import Text from "../components/Text";
+import LinkHL from "../components/LinkHL";
+
+const Help: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>bukuniki / dashboard</title>
+        <meta name="description" content="Dashboard of bukuniki" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Layout className="w-9/12">
+          <section>
+            <h1 className="text-5xl mb-2">bukuniki Help</h1>
+            <Text>
+              There might be a bug or an issue that you have encountered here,
+              and we would be very happy to help you with that. There are
+              several ways how you can reach help at bukuniki:
+            </Text>
+            <ul>
+              <li>
+                By trying to find the same Issue on our{" "}
+                <LinkHL href="https://github.com/gibsol/bukuniki">
+                  Github
+                </LinkHL>{" "}
+              </li>
+              <li>
+                By submitting a new Issue. In that case, we encourage you to
+                check our{" "}
+                {/* todo: add a link to the contribution guide when it's gonna be written */}
+                <LinkHL href="">CONTRIBUTION.md</LinkHL>
+              </li>
+            </ul>
+          </section>
+
+          <section className="mt-3">
+            <h2 className="text-2xl mb-2">
+              What If I do not have a Github account?
+            </h2>
+            <Text>
+              Well, In that case you would probably want to connect us directly
+              via email. However, this option is not available yet and will be
+              added in future updates.
+            </Text>
+            <Text>
+              You can follow the development process on our{" "}
+              <LinkHL href="https://github.com/gibsol/bukuniki">
+                Github page
+              </LinkHL>
+              .
+            </Text>
+          </section>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default Help;
