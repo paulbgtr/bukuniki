7d63304ce831520d01473e172417a022d4a5a8f5
Gibsol <gibsol75@gmail.com>
2023-01-12 14:12:05 +0200

    :poop: Added some Journals for testing purposes
    Added JournalContainer component for Journals.
    Added some journals to the my-journals page to test them.
    
    Added also a new styled Header to it.

--- pages/my-journals.tsx
+++ pages/my-journals.tsx
@@ -1,5 +1,7 @@
-import Navbar from "../components/Navbar";
 import Head from "next/head";
+import Layout from "../components/Layout";
+import Journal from "../pageComponents/my-journals/Journal";
+import JournalContainer from "../pageComponents/my-journals/JournalContainer";
 
 const MyJournals = () => {
   return (
@@ -11,10 +13,20 @@ const MyJournals = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
-
       <main>
-        <h1>My Journals</h1>
+        <Layout>
+          <h1 className="text-4xl mb-2 font-bold">My Journals</h1>
+          <JournalContainer>
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+            <Journal href="" title="Anna Karenina" author="Leo Tolstoy" />
+          </JournalContainer>
+        </Layout>
       </main>
     </>
   );
