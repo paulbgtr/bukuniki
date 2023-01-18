5c6d24d048c58c5ed7c9d6ff86c96e46ef9dae97
Gibsol <gibsol75@gmail.com>
2023-01-18 16:33:32 +0200

    :sparkles: User navbar dropdown
    Added a new Dropdown component that is called when clicked on a <button> inside the <ProfileImage />.
    Its made to help person to navigate through the app and search for user
    settings, Logout, etc.

---
+++ components/Navbar/Dropdown.tsx
@@ -0,0 +1,41 @@
+// A simple dropdown component that is called from the ProfileImage component.
+
+// todo: basic functionality:
+// - forward to the profile page
+// - forward to the settings page
+// - sign out
+
+import React, { FC } from "react";
+
+const Dropdown: FC = () => {
+  return (
+    <div className="absolute right-5 top-3 w-48 mt-2 origin-top-right divide-y rounded-xl shadow-lg outline-none">
+      <div className="px-4 py-3">
+        <p className="text-sm text-gray-400">Signed in as</p>
+        <p className="text-sm font-medium text-white truncate">Username</p>
+      </div>
+      <div className="py-1">
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm duration-200 text-white hover:bg-gray-100 hover:text-gray-900"
+        >
+          Your Profile
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm duration-200 text-white hover:bg-gray-100 hover:text-gray-900"
+        >
+          Settings
+        </a>
+        <a
+          href="#"
+          className="block px-4 py-2 text-sm duration-200 text-white hover:bg-gray-100 hover:text-gray-900"
+        >
+          Sign out
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
