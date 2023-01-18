346a4eb3be50d047300e8800fe2c2e83435fa4a5
Gibsol <gibsol75@gmail.com>
2023-01-18 17:15:55 +0200

    :children_crossing: Removed Navbar from signup and login pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Navbar from "./Navbar/Navbar";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 type Props = {
   children: React.ReactNode;
@@ -9,10 +10,16 @@ type Props = {
 
 const Layout: FC<Props> = (props) => {
   const { asPath } = useRouter();
+  const [rightPath, setRightPath] = useState(false);
 
   return (
     <>
-      {asPath !== "/" && <Navbar />}
+      {/* Checking if the path is not equal to "/" or "/login" or "/signup" and if
+      it is not then it will render the Navbar component. */}
+      {asPath !== "/" && asPath !== "/login" && asPath !== "/signup" && (
+        <Navbar />
+      )}
+
       <div className={`${props.className} container md:my-16 mx-auto`}>
         {props.children}
       </div>
