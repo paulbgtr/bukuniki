fbb4769b8c9a47474d84d81166ffc6e505906b09
Gibsol <gibsol75@gmail.com>
2023-01-18 16:32:48 +0200

    :sparkles: ProfileImage Dropdown component
    Added a component that includes a button with an Image of a user inside. When clicked, the dropdown is called.

---
+++ components/Navbar/ProfileImage.tsx
@@ -0,0 +1,27 @@
+// todo: make the component responsive
+
+import React, { FC } from "react";
+import Image from "next/image";
+import { useState } from "react";
+import Dropdown from "./Dropdown";
+import profileImage from "../../public/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.jpeg";
+
+const ProfileImage: FC = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  return (
+    <button
+      onClick={() => setDropdown(!dropdown)}
+      className="relative w-10 h-10"
+    >
+      {dropdown ? <Dropdown /> : null}
+      <Image
+        alt="User Profile image"
+        src={profileImage}
+        className="rounded-full"
+      />
+    </button>
+  );
+};
+
+export default ProfileImage;
