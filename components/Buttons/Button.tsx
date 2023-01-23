a2959c0db6f24e0166ccef74580f307e0d5c76af
Gibsol <gibsol75@gmail.com>
2023-01-23 18:31:05 +0200

    :lipstick: Added a new "outlined" variant for the Button

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -4,16 +4,28 @@ interface Props {
   children: React.ReactNode;
   className?: string;
   onclick?: () => void;
+  variant?: "outlined" | "filled";
 }
 
 const Button: FC<Props> = (props) => {
   return (
-    <button
-      onClick={props.onclick}
-      className={`${props.className} duration-300 py-3 px-5 rounded-xl font-bold text-lg hover:opacity-70 bg-pink-300`}
-    >
-      {props.children}
-    </button>
+    <>
+      {props.variant === "outlined" ? (
+        <button
+          onClick={props.onclick}
+          className={`${props.className} duration-300 py-3 px-5 rounded-xl font-bold border-2 text-sm hover:opacity-70 border-pink-300 bg-white-300`}
+        >
+          {props.children}
+        </button>
+      ) : (
+        <button
+          onClick={props.onclick}
+          className={`${props.className} duration-300 py-3 px-5 rounded-xl border-2 border-pink-300 font-bold text-sm hover:opacity-70 bg-pink-300`}
+        >
+          {props.children}
+        </button>
+      )}
+    </>
   );
 };
 
