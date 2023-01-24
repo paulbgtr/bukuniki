39790d5c00dabf34ca69b07d16c1b598c92f03a8
Gibsol <gibsol75@gmail.com>
2023-01-24 17:07:10 +0200

    :recycle: Refactoring of the Button component
    Refactored the button component with a normal switch statement that will return a different variant of style that depends of the props.variant

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -8,24 +8,22 @@ interface Props {
 }
 
 const Button: FC<Props> = (props) => {
+  let classes = "";
+
+  switch (props.variant) {
+    case "outlined":
+      classes =
+        "duration-300 py-3 px-5 rounded-xl border-2 border-pink-300 font-bold text-sm hover:opacity-70 bg-white-300";
+      break;
+    default:
+      classes =
+        "duration-300 py-3 px-5 rounded-xl border-2 border-pink-300 font-bold text-sm hover:opacity-70 bg-pink-300";
+  }
+
   return (
-    <>
-      {props.variant === "outlined" ? (
-        <button
-          onClick={props.onclick}
-          className={`${props.className} duration-300 py-3 px-5 rounded-xl font-bold border-2 text-sm hover:opacity-70 border-pink-300 bg-white-300`}
-        >
-          {props.children}
-        </button>
-      ) : (
-        <button
-          onClick={props.onclick}
-          className={`${props.className} duration-300 py-3 px-5 rounded-xl border-2 border-pink-300 font-bold text-sm hover:opacity-70 bg-pink-300`}
-        >
-          {props.children}
-        </button>
-      )}
-    </>
+    <button onClick={props.onclick} className={`${props.className} ${classes}`}>
+      {props.children}
+    </button>
   );
 };
 
