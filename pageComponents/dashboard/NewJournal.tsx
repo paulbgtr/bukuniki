416533313460bd04019523a5667ebd036cddaf08
Gibsol <gibsol75@gmail.com>
2023-01-24 18:27:45 +0200

    :iphone: Responsivness of the NewJournal
    Removed unusable div.
    Made the component more responsive by settings it to w-full.

--- pageComponents/dashboard/NewJournal.tsx
+++ pageComponents/dashboard/NewJournal.tsx
@@ -59,36 +59,34 @@ const NewJournal: FC = () => {
   };
 
   return (
-    <div>
-      <Card title="New Journal">
-        <form onSubmit={handleSubmit} action="">
-          <div className="flex-wrap flex justify-center">
-            <input
-              placeholder="Book's Author"
-              className="w-80 border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-              type="text"
-              name="author"
-              value={state.author}
-              onChange={handleChange}
-              required
-            />
-            <input
-              placeholder="Book's Title"
-              className="w-80 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
-              type="text"
-              name="title"
-              value={state.title}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="mt-5">
-            <AddJournal formFilled={isFormFilled} />
-          </div>
-        </form>
-        {isFormFilled() && <Notification />}
-      </Card>
-    </div>
+    <Card title="New Journal">
+      <form onSubmit={handleSubmit} action="">
+        <div className="w-full">
+          <input
+            placeholder="Book's Author"
+            className="w-full border-2 mt-3 mb-3 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+            type="text"
+            name="author"
+            value={state.author}
+            onChange={handleChange}
+            required
+          />
+          <input
+            placeholder="Book's Title"
+            className="w-full border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+            type="text"
+            name="title"
+            value={state.title}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mt-5">
+          <AddJournal formFilled={isFormFilled} />
+        </div>
+      </form>
+      {isFormFilled() && <Notification />}
+    </Card>
   );
 };
 
