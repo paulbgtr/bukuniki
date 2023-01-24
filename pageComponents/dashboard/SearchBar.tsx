5a72d777e98c769693407ff6990f6678fbfa4cab
Gibsol <gibsol75@gmail.com>
2023-01-24 18:26:32 +0200

    :bulb: Searchbar comments and width
    Made SearchBar's width = full and added a todo comment

--- pageComponents/dashboard/SearchBar.tsx
+++ pageComponents/dashboard/SearchBar.tsx
@@ -1,10 +1,11 @@
+// todo: basic functionality for search bar
 import React, { FC } from "react";
 
 const SearchBar: FC = () => {
   return (
     <input
       placeholder="Search for journals..."
-      className="w-96 border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
+      className="w-full border-2 border-pink-300 focus:outline-none pl-3 rounded-xl py-2"
       type="text"
     />
   );
