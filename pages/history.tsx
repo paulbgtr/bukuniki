3eb3aa0422db2b249859c6464129ce0e6c9e38db
Gibsol <gibsol75@gmail.com>
2023-01-28 12:14:16 +0200

    :construction: Added a new yet blank page of history

---
+++ pages/history.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const History = () => {
+  return (
+    <>
+      <Head>
+        <title>bukuniki / history</title>
+        <meta name="description" content="Online reading journal." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </>
+  );
+};
+
+export default History;
