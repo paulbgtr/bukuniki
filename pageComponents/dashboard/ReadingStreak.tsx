9e7d4a600115fca34260cb85dcd16481640f4c7f
Gibsol <gibsol75@gmail.com>
2023-01-28 12:14:51 +0200

    :sparkles: OnHold -> goto /history
    Added a functionality that pushes /history to router after holding the <ReadingStreak />

--- pageComponents/dashboard/ReadingStreak.tsx
+++ pageComponents/dashboard/ReadingStreak.tsx
@@ -1,11 +1,17 @@
-// todo: flip card onclick to show streak history
-
 import React from "react";
 import { FC } from "react";
 import { useState } from "react";
 import Card from "../../components/Card";
+import { useLongPress } from "use-long-press";
+import { useRouter } from "next/router";
 
 const ReadingStreak: FC = () => {
+  const router = useRouter();
+
+  const bind = useLongPress(() => {
+    router.push("/history");
+  });
+
   const [flip, setFlip] = useState(false);
 
   const cardFlip = () => {
@@ -13,7 +19,7 @@ const ReadingStreak: FC = () => {
   };
 
   return (
-    <button onClick={cardFlip} className="text-left">
+    <button {...bind()} onClick={cardFlip} className="text-left">
       {flip ? (
         <Card id="streak" textSize="7xl" title="Current Reading Streak">
           History
