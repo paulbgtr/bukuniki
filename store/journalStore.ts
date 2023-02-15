8812d2f8b327c0294dec77e1dc0dd2bcd83d5d8f
Gibsol <gibsol75@gmail.com>
2023-02-15 16:23:55 +0200

    :construction: JournalStore update
    Added a Journal State as well as a new function to add new journal to the array of journals

--- store/journalStore.ts
+++ store/journalStore.ts
@@ -1,5 +1,13 @@
 import { create } from "zustand";
+import { Journal } from "../types/Journal";
 
-const useJournalStore = create((set) => ({
+type JournalState = {
+  journals: Journal[];
+  addJournals(journal: Journal): void;
+};
+
+const useJournalStore = create<JournalState>()((set) => ({
   journals: [],
+  addJournals: (journal: Journal) =>
+    set((state) => ({ journals: [...state.journals, journal] })),
 }));
