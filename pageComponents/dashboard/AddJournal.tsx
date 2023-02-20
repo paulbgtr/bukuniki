44b38e218ef0e5dc54648e225449b42d1286bfc5
Gibsol <gibsol75@gmail.com>
2023-02-20 20:26:04 +0200

    :technologist: Make a separate file for AddJournal component

---
+++ pageComponents/dashboard/AddJournal.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { notify } from "../../utils/notify";
+
+type AddJournalProps = {
+  formFilled: () => boolean;
+};
+
+// ? "plus sign button" that appears in the NewJournal component.
+const AddJournal = ({ formFilled }: AddJournalProps) => {
+  const handleClick = () => {
+    if (formFilled()) {
+      notify("Journal Added!");
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className="px-4 pt-1 pb-2 mt-5 text-3xl text-white duration-300 bg-pink-300 rounded-full hover:opacity-70"
+    >
+      +
+    </button>
+  );
+};
+
+export default AddJournal;
--- pageComponents/dashboard/NewJournal.tsx
+++ pageComponents/dashboard/NewJournal.tsx
@@ -2,29 +2,7 @@ import React, { useState } from "react";
 
 import Card from "../../components/Card";
 import Notification from "../../components/Notification";
-import { notify } from "../../utils/notify";
-
-type AddJournalProps = {
-  formFilled: () => boolean;
-};
-
-// ? "plus sign button" that appears in the NewJournal component.
-const AddJournal = ({ formFilled }: AddJournalProps) => {
-  const handleClick = () => {
-    if (formFilled()) {
-      notify("Journal Added!");
-    }
-  };
-
-  return (
-    <button
-      onClick={handleClick}
-      className="px-4 pt-1 pb-2 mt-5 text-3xl text-white duration-300 bg-pink-300 rounded-full hover:opacity-70"
-    >
-      +
-    </button>
-  );
-};
+import AddJournal from "./AddJournal";
 
 const NewJournal = () => {
   const [state, setState] = useState({
