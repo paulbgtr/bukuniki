a3be273f51fd4d723eb2c891bae588a3421b4fa8
Gibsol <gibsol75@gmail.com>
2023-02-20 19:57:01 +0200

    :fire: Remove information and some dead code on contact
    Remove unecessary section tag and contact form from the contact page.
    Remove the width class from Layout

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import Layout from "../components/Layout";
-import Text from "../components/Text";
-import LinkHL from "../components/Links/LinkHL";
-import ContactForm from "../pageComponents/contact/ContactForm";
 
-const Contact = () => {
+import Layout from "../components/Layout";
+import LinkHL from "../components/Links/LinkHL";
+import Text from "../components/Text";
+
+const Help = () => {
   return (
     <>
       <Head>
@@ -15,59 +15,32 @@ const Contact = () => {
       </Head>
 
       <main>
-        <Layout className="w-9/12">
-          <section>
-            <h1 className="text-4xl text-center mb-5 text-white-300 font-bold">
-              Contact bukuniki
-            </h1>
-            <Text>
-              There might be a bug or an issue that you have encountered here,
-              and we would be very happy to help you with that. There are
-              several ways how you can reach help at bukuniki:
-            </Text>
-            <ul>
-              <li className="text-lg">
-                - By trying to find the same Issue on our{" "}
-                <LinkHL href="https://github.com/gibsol/bukuniki">
-                  Github
-                </LinkHL>{" "}
-              </li>
-              <li className="text-lg">
-                - By submitting a new Issue. In that case, we encourage you to
-                check our{" "}
-                {/* todo: add a link to the contribution guide when it's gonna be written */}
-                <LinkHL href="">CONTRIBUTION.md</LinkHL> before submitting a new
-                issue.
-              </li>
-            </ul>
-          </section>
-
-          <section className="mb-3 mt-5">
-            <h2 className="text-2xl text-pink-300 mb-2 font-bold">
-              What If I do not have a GitHub account?
-            </h2>
-            <Text>
-              Well, In that case you would probably want to connect us directly
-              via email. However, this option is not available yet and will be
-              added in future updates.
-            </Text>
-            <Text>
-              While not having a GitHub account, you can follow the development
-              process on our{" "}
-              <LinkHL href="https://github.com/gibsol/bukuniki">
-                GitHub page
-              </LinkHL>
-              .
-            </Text>
-          </section>
-
-          {/* <section className="mt-10 flex justify-center">
-            <ContactForm />
-          </section> */}
+        <Layout>
+          <h1 className="mb-5 text-4xl font-bold text-center text-white-300">
+            bukuniki: Help
+          </h1>
+          <Text>
+            There might be a bug or an issue that you have encountered here, and
+            we would be very happy to help you with that. There are several ways
+            how you can reach help at bukuniki:
+          </Text>
+          <ul>
+            <li className="text-lg">
+              By finding that Issue on our{" "}
+              <LinkHL href="https://github.com/gibsol/bukuniki">Github</LinkHL>{" "}
+            </li>
+            <li className="text-lg">
+              By submitting a new Issue. In that case, we encourage you to check
+              our{" "}
+              {/* todo: add a link to the contribution guide when it's gonna be written */}
+              <LinkHL href="">CONTRIBUTION.md</LinkHL> before submitting a new
+              issue.
+            </li>
+          </ul>
         </Layout>
       </main>
     </>
   );
 };
 
-export default Contact;
+export default Help;
